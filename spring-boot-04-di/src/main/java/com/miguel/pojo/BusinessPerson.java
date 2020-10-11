07cc299f3e18f24e9466663e1576641209986a32
zx_panjiaming <595156223@qq.com>
2020-10-11 11:56:29 +0800

    @Primary & @Qualifier

--- spring-boot-04-di/src/main/java/com/miguel/pojo/BusinessPerson.java
+++ spring-boot-04-di/src/main/java/com/miguel/pojo/BusinessPerson.java
@@ -1,6 +1,7 @@
 package com.miguel.pojo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,18 +12,24 @@ public class BusinessPerson implements Person {
     // private Animal animal = null;
 
     // 容器会先根据类型判断，再根据属性名判断，最终确认注入的bean
+    // 但是修改变量名是比较憋屈的做法
+    // @Autowired
+    // private Animal dog = null;
+
+    // @Autowired与@Qualifier相结合，让容器根据类型和名称来寻找bean，就能消除歧义性了。
     @Autowired
-    private Animal dog = null;
+    @Qualifier("dog")
+    private Animal animal = null;
 
     @Override
     public void service() {
-        // this.animal.use();
-        this.dog.use();
+        this.animal.use();
+        // this.dog.use();
     }
 
     @Override
     public void setAnimal(Animal animal) {
-        // this.animal = animal;
-        this.dog = animal;
+        this.animal = animal;
+        // this.dog = animal;
     }
 }
